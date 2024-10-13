4f8fca2de433ffb630837b33435e90f713e938f2
Elizaveta <liza.bananana@gmail.com>
2024-10-13 21:42:33 +0400

    added parsing 5 category pages

--- parser.js
+++ parser.js
@@ -6,39 +6,42 @@ function delay(time) {
   });
 }
 
-async function findInfoAboutProduct(page, link) {
-  // await delay(2000); // задержка 2 секунды
-  await page.goto(link, { waitUntil: "networkidle2" }); // ожидание полной загрузки страницы
+async function findInfoAboutProduct(browser, link) {
+  await delay(1000); // задержка 1 секунда
+  const page = await browser.newPage();
+  try {
+    await page.goto(link, { waitUntil: "networkidle2" }); // ожидание полной загрузки страницы
 
-  const title = await page
-    .$eval('[data-marker="item-view/title-info"]', (el) => el.innerText)
-    .catch(() => null);
-  const price = await page
-    .$eval('[data-marker="item-view/item-price"]', (el) => el.innerText)
-    .catch(() => null);
-  const marketer = await page
-    .$eval('[data-marker="seller-link/link"]', (el) => el.innerText)
-    .catch(() => null);
-  const creationDate = await page
-    .$eval('[data-marker="item-view/item-date"]', (el) => el.innerText)
-    .catch(() => null);
-  const totalViews = await page
-    .$eval('[data-marker="item-view/total-views"]', (el) => el.innerText)
-    .catch(() => null);
-  const todayViews = await page
-    .$eval('[data-marker="item-view/today-views"]', (el) => el.innerText)
-    .catch(() => null);
+    const title = await page
+      .$eval('[data-marker="item-view/title-info"]', (el) => el.innerText)
+      .catch(() => null);
+    const price = await page
+      .$eval('[data-marker="item-view/item-price"]', (el) => el.innerText)
+      .catch(() => null);
+    const marketer = await page
+      .$eval('[data-marker="seller-link/link"]', (el) => el.innerText)
+      .catch(() => null);
+    const creationDate = await page
+      .$eval('[data-marker="item-view/item-date"]', (el) => el.innerText)
+      .catch(() => null);
+    const totalViews = await page
+      .$eval('[data-marker="item-view/total-views"]', (el) => el.innerText)
+      .catch(() => null);
+    const todayViews = await page
+      .$eval('[data-marker="item-view/today-views"]', (el) => el.innerText)
+      .catch(() => null);
 
-  // await delay(2000); // дополнительная задержка
-
-  return {
-    title,
-    price,
-    marketer,
-    creationDate,
-    totalViews,
-    todayViews,
-  };
+    return {
+      title,
+      price,
+      marketer,
+      creationDate,
+      totalViews,
+      todayViews,
+    };
+  } finally {
+    await page.close(); // закрываем страницу после парсинга
+  }
 }
 
 (async () => {
@@ -61,27 +64,33 @@ async function findInfoAboutProduct(page, link) {
     }
   });
 
-  await page.goto(url, { waitUntil: "networkidle2" });
+  const pagesInfo = [];
+  for (let i = 1; i <= 5; i++) {
+    await page.goto(`${url}?p=${i}`, { waitUntil: "networkidle2" });
 
-  const products = [];
-  const links = [];
-  const items = await page.$$('div[data-marker="item"]'); // Получаем все элементы товаров
+    const links = [];
+    const items = await page.$$('div[data-marker="item"]'); // Получаем все элементы товаров
 
-  for (let i = 0; i < items.length; i++) {
-    const product = items[i]; // Получаем текущий элемент
-    const link = await product.$eval("a", (a) => a.href).catch(() => null); // Извлекаем ссылку
+    for (let i = 0; i < items.length; i++) {
+      const product = items[i]; // Получаем текущий элемент
+      const link = await product.$eval("a", (a) => a.href).catch(() => null); // Извлекаем ссылку
 
-    if (link) {
-      links.push(link);
+      if (link) {
+        links.push(link);
+      }
     }
-  }
 
-  for (let i = 0; i < links.length; i++) {
-    const infoProduct = await findInfoAboutProduct(page, links[i]);
-    products.push(infoProduct);
-  }
+    const pLimit = await import("p-limit"); // Динамический импорт
+    const limit = pLimit.default(5); // Ограничение до 5 параллельных запросов
 
-  console.log(products);
+    const infoPromises = links.map((link) =>
+      limit(() => findInfoAboutProduct(browser, link))
+    );
+    const productsInfo = await Promise.all(infoPromises);
+
+    console.log(productsInfo);
+    pagesInfo.push(productsInfo);
+  }
 
   await browser.close();
 })();
